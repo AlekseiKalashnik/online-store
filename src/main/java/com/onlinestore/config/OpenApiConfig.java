31e81b1b95789f272e47075e1b35e773843f7a49
AlekseiKalashnik <id50538996@gmail.com>
2023-08-19 20:11:12 +0300

    reorganize openAPI

--- src/main/java/com/onlinestore/config/OpenApiConfig.java
+++ src/main/java/com/onlinestore/config/OpenApiConfig.java
@@ -1,14 +1,24 @@
 package com.onlinestore.config;
 
 import io.swagger.v3.oas.annotations.OpenAPIDefinition;
+import io.swagger.v3.oas.annotations.info.Contact;
 import io.swagger.v3.oas.models.OpenAPI;
 import io.swagger.v3.oas.models.info.Info;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
-@OpenAPIDefinition
-public class SwaggerConfig {
+@OpenAPIDefinition(
+        info = @io.swagger.v3.oas.annotations.info.Info(
+                title = "Online store",
+                description = "Test app", version = "1.0.0",
+                contact = @Contact(
+                        name = "Alex K",
+                        email = "id50538996@gmail.com"
+                )
+        )
+)
+public class OpenApiConfig {
 
     @Bean
     public OpenAPI baseOpenApi() {
